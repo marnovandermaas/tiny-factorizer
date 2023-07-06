86bed931b7d741c803b19d50b2a1811ae7ce9c4f
Marno van der Maas <mvdmaas+git@lowrisc.org>
2023-07-06 16:21:05 +0100

    Cleanup of seven segment top

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -6,50 +6,52 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 10_000_000 ) (
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
     output wire [7:0] uio_out,  // IOs: Bidirectional Output path
     output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire       ena,      // Will go high when the design is enabled
+    input  wire       clk,      // Clock
+    input  wire       rst_n     // reset_n - Low to reset
 );
 
-    wire reset = ! rst_n;
+    wire reset = !rst_n;
     wire [6:0] led_out;
-    assign uo_out[6:0] = led_out;
+    assign uo_out[6:0] = led_out; // Only least significant 7 bits are used
 
-    // use bidirectionals as outputs
+    // Use bidirectionals as outputs
     assign uio_oe = 8'b11111111;
 
-    // put bottom 8 bits of second counter out on the bidirectional gpio
+    // Put bottom 8 bits of second counter out on the bidirectional gpio
     assign uio_out = second_counter[7:0];
 
-    // external clock is 10MHz, so need 24 bit counter
+    // External clock is 10MHz, so need 24 bit counter
     reg [23:0] second_counter;
     reg [3:0] digit;
 
     always @(posedge clk) begin
-        // if reset, set counter to 0
+        // If reset, set counter and digit to 0
         if (reset) begin
             second_counter <= 0;
             digit <= 0;
         end else begin
-            // if up to 16e6
-            if (second_counter == MAX_COUNT) begin
-                // reset
+            // If counted up to second
+            if (second_counter == MAX_COUNT - 1) begin
+                // Reset counter
                 second_counter <= 0;
 
-                // increment digit
+                // Increment digit
                 digit <= digit + 1'b1;
 
-                // only count from 0 to 9
-                if (digit == 9)
+                // Only count from 0 to 9
+                if (digit == 9) begin
                     digit <= 0;
+                end
 
-            end else
-                // increment counter
+            end else begin
+                // Increment counter
                 second_counter <= second_counter + 1'b1;
+            end
         end
     end
 
-    // instantiate segment display
+    // Instantiate segment display
     seg7 seg7(.counter(digit), .segments(led_out));
 
 endmodule
