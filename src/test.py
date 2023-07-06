23ce15c1e07511d337c1212414bf425e1c487b6f
Marno van der Maas <mvdmaas+git@lowrisc.org>
2023-07-06 16:37:28 +0100

    Add comments to test.py

--- src/test.py
+++ src/test.py
@@ -2,15 +2,17 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
-
+# Expected 7 segment outputs
 segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
 
 @cocotb.test()
 async def test_7seg(dut):
+    # Start the clock
     dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
+    # Reset the design
     dut._log.info("reset")
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
@@ -22,5 +24,5 @@ async def test_7seg(dut):
         await ClockCycles(dut.clk, 1000)
         assert int(dut.segments.value) == segments[i]
 
-        # all bidirectionals are set to output
+        # All bidirectionals are set to output
         assert dut.uio_oe == 0xFF
