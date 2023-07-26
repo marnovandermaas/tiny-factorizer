03b591b3374c151fe6480e763f25802d92517e44
Marno van der Maas <mvdmaas+git@lowrisc.org>
2023-07-26 09:40:32 +0100

    See if GL is fixed when max count is hardcoded to 1000

--- src/tt_um_marno_factorize.v
+++ src/tt_um_marno_factorize.v
@@ -1,6 +1,7 @@
 `default_nettype none
 
-module tt_um_marno_factorize #( parameter MAX_COUNT = 24'd10_000_000 ) (
+// TODO change max count bak to 10 million
+module tt_um_marno_factorize #( parameter MAX_COUNT = 24'd00_001_000 ) (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
